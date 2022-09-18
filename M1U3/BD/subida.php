a789d394f803682f7a6b7f92856be4281904d416
Hernan Carrizo <carrizohf35@gmail.com>
2022-09-18 10:04:52 -0300

    Se agrego css a Formulario

--- M1U3/BD/subida.php
+++ M1U3/BD/subida.php
@@ -1,27 +1,41 @@
 <?php
 
-$host= "localhost";
+$host = "localhost";
 $usuario = "root";
 $clave = "";
 $bd = "fullstack";
 
-$conexion = mysqli_connect($host, $usuario,$clave,$bd)
-or die(mysqli_error($mysquli));
+$conexion = mysqli_connect($host, $usuario, $clave, $bd)
+	or die(mysqli_error($mysquli));
 
-if($conexion)
-{ echo "Conectado";}
-else{
+if ($conexion) {
+	echo "Conectado";
+} else {
 	echo "No se pudo conectar";
 }
 
 $Nombre = $_POST['Nombre'];
 $Apellido = $_POST['Apellido'];
 $Telefono = $_POST['Telefono'];
+$resultado = 0;
 
-$consulta = "INSERT INTO consultascv(Nombre, Apellido, Telefono) 
-VALUES ('$Nombre','$Apellido','$Telefono')";
+if (isset($_POST['Enviar'])) {
+	if (strlen($_POST['Nombre']) >= 1 && strlen($_POST['Apellido']) >= 1 && strlen($_POST['Telefono']) >= 1) {
+		$consulta = "INSERT INTO consultascv(Nombre, Apellido, Telefono) VALUES ('$Nombre','$Apellido','$Telefono')";
+		$resultado = mysqli_query($conexion, $consulta);
+	}
+}
 
-mysqli_query($conexion,$consulta);
+if ($resultado) {
+?>
+	<h3>Te has inscripto correctamente!!</h3>
+<?php
+} else {
+?>
+	<h3>Debe completar todos los campos</h3>
+<?php
+}
+
 mysqli_close($conexion);
 
 ?>
