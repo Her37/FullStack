37f06fe58c13a726c93fae6b055032fd889c012c
Hernan Carrizo <carrizohf35@gmail.com>
2022-11-21 11:58:12 -0300

    React

---
+++ M4U1/Proyecto_Inmobiliario/frontend/src/pages/Novedades.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const Novedades = (props) => {
+    return (
+        <main className="holder">
+
+            <h2>NOVEDADES</h2>
+            {/* <br> </br> */}
+            <div className="novedades">
+                <h3>Titulo</h3>
+                {/* <br> </br> */}
+                <h4>Subtitulo</h4>
+                {/* <br> </br> */}
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, deserunt quae aspernatur laborum officia
+                    cumque quia quidem fugiat necessitatibus iusto excepturi quo placeat rerum! Inventore ab sed velit
+                    praesentium expedita?</p>
+                {/* <hr> </hr>
+                <br> </br> */}
+                <h3>Titulo</h3>
+                {/* <br> </br> */}
+                <h4>Subtitulo</h4>
+                {/* <br> </br> */}
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, deserunt quae aspernatur laborum officia
+                    cumque quia quidem fugiat necessitatibus iusto excepturi quo placeat rerum! Inventore ab sed velit
+                    praesentium expedita?</p>
+                {/* <hr> </hr>
+                <br> </br> */}
+                <h3>Titulo</h3>
+                {/* <br> </br> */}
+                {/* <h4>Subtitulo</h4>
+                <br> </br> */}
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, deserunt quae aspernatur laborum officia
+                    cumque quia quidem fugiat necessitatibus iusto excepturi quo placeat rerum! Inventore ab sed velit
+                    praesentium expedita?</p>
+                {/* <hr></hr>
+                <br></br> */}
+            </div>
+        </main>
+
+    );
+};
+
+export default Novedades;
