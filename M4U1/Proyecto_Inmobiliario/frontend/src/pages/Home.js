abff9c22d7fb89cd170543e05660e3d36f0f0a58
Hernan Carrizo <carrizohf35@gmail.com>
2022-12-16 21:22:26 -0300

    boton central home

--- M4U1/Proyecto_Inmobiliario/frontend/src/pages/Home.js
+++ M4U1/Proyecto_Inmobiliario/frontend/src/pages/Home.js
@@ -2,81 +2,97 @@ import React from 'react';
 import '../styles/home.css';
 
 const Home = (props) => {
-    return (
-        <main>
-<div className='caja'>
-            <img className='fondo' src="../../../img/home/Home.jpg" alt="" />
- </div>
-            <div className="holder">
-                <div className="columnas" >
+return (
+<main>
+    <div className="inicio">
+        <div id="caja">
+            <form id="form">
+                <input type="text" id="barra" />
+                <button className="acordion">buscar</button>
+                <button className="acordion">lugar</button>
+                <button className="acordion">tipo</button>
+                <button className="acordion">alquiler</button>
+            </form>
+        </div>
+        <img className="fondo" src="../../../img/home/Home.jpg" alt="" />
 
-                    <section className="bienvenidos">
-                        <h2>Bienvenidos</h2>
-                        <br />
-                        <p><b>Bienvenidos, este sito conciste en una plataforma de intercambios sin intermediarios. Donde las
-                            persoanas podran intercambiar libremente lo que deseen.
-                            Para ello en un futuro implementaremos el uso de la tecnologia de Contratos Inteligentes o
-                            <span> SamrtContract</span>, pero que actualemte no se encuentra disponible estamos trabajando en su
-                            implementacion.</b>
-                        </p>
-                        <br />
-                        <p><b>Por el contrario, podras consultar y ponerte en contacto con los distintos ofertantes del sito a
-                            travez de email o telefono y pactar la forma de intercambio que desees.
-                            Ten en cuenta que no se permitira publicar articulos sin su debida documentacion; como ser: titulo, certificados,
-                            factura, etc. ni tampoco ningun articulo prohibido por las leyes actuales.</b>
-                        </p>
-                        <br />
-                        <p><b>Si lo deseas te invito a que te des una vuelta por el Sitio y me dejes una opinion en los
-                            comentarios. Saludos!</b></p>
+    </div>
+    <div className="holder">
+        <div className="columnas">
 
-                        <br />
-                        <p><b>Bienvenidos, este sito conciste en una plataforma de intercambios sin intermediarios. Donde las
-                            persoanas podran intercambiar libremente lo que deseen.
-                            Para ello en un futuro implementaremos el uso de la tecnologia de Contratos Inteligentes o
-                            <span> SamrtContract</span>, pero que actualemte no se encuentra disponible estamos trabajando en su
-                            implementacion.</b>
-                        </p>
-                        <br />
-                        <p><b>Por el contrario, podras consultar y ponerte en contacto con los distintos ofertantes del sito a
-                            travez de email o telefono y pactar la forma de intercambio que desees.
-                            Ten en cuenta que no se permitira publicar articulos sin su debida documentacion; como ser: titulo, certificados,
-                            factura, etc. ni tampoco ningun articulo prohibido por las leyes actuales.</b>
-                        </p>
-                        <br />
-                        <p><b>Si lo deseas te invito a que te des una vuelta por el Sitio y me dejes una opinion en los
-                            comentarios. Saludos!</b></p>
+            <section className="bienvenidos">
+                <h2>Bienvenidos</h2>
+                <br />
+                <p><b>Bienvenidos, este sito conciste en una plataforma de intercambios sin intermediarios. Donde las
+                        persoanas podran intercambiar libremente lo que deseen.
+                        Para ello en un futuro implementaremos el uso de la tecnologia de Contratos Inteligentes o
+                        <span> SamrtContract</span>, pero que actualemte no se encuentra disponible estamos trabajando
+                        en su
+                        implementacion.</b>
+                </p>
+                <br />
+                <p><b>Por el contrario, podras consultar y ponerte en contacto con los distintos ofertantes del sito a
+                        travez de email o telefono y pactar la forma de intercambio que desees.
+                        Ten en cuenta que no se permitira publicar articulos sin su debida documentacion; como ser:
+                        titulo, certificados,
+                        factura, etc. ni tampoco ningun articulo prohibido por las leyes actuales.</b>
+                </p>
+                <br />
+                <p><b>Si lo deseas te invito a que te des una vuelta por el Sitio y me dejes una opinion en los
+                        comentarios. Saludos!</b></p>
 
-                        <br />
-                        <p><b>Bienvenidos, este sito conciste en una plataforma de intercambios sin intermediarios. Donde las
-                            persoanas podran intercambiar libremente lo que deseen.
-                            Para ello en un futuro implementaremos el uso de la tecnologia de Contratos Inteligentes o
-                            <span> SamrtContract</span>, pero que actualemte no se encuentra disponible estamos trabajando en su
-                            implementacion.</b>
-                        </p>
-                        <br />
-                        <p><b>Por el contrario, podras consultar y ponerte en contacto con los distintos ofertantes del sito a
-                            travez de email o telefono y pactar la forma de intercambio que desees.
-                            Ten en cuenta que no se permitira publicar articulos sin su debida documentacion; como ser: titulo, certificados,
-                            factura, etc. ni tampoco ningun articulo prohibido por las leyes actuales.</b>
-                        </p>
-                        <br />
-                        <p><b>Si lo deseas te invito a que te des una vuelta por el Sitio y me dejes una opinion en los
-                            comentarios. Saludos!</b></p>
+                <br />
+                <p><b>Bienvenidos, este sito conciste en una plataforma de intercambios sin intermediarios. Donde las
+                        persoanas podran intercambiar libremente lo que deseen.
+                        Para ello en un futuro implementaremos el uso de la tecnologia de Contratos Inteligentes o
+                        <span> SamrtContract</span>, pero que actualemte no se encuentra disponible estamos trabajando
+                        en su
+                        implementacion.</b>
+                </p>
+                <br />
+                <p><b>Por el contrario, podras consultar y ponerte en contacto con los distintos ofertantes del sito a
+                        travez de email o telefono y pactar la forma de intercambio que desees.
+                        Ten en cuenta que no se permitira publicar articulos sin su debida documentacion; como ser:
+                        titulo, certificados,
+                        factura, etc. ni tampoco ningun articulo prohibido por las leyes actuales.</b>
+                </p>
+                <br />
+                <p><b>Si lo deseas te invito a que te des una vuelta por el Sitio y me dejes una opinion en los
+                        comentarios. Saludos!</b></p>
 
-                    </section>
+                <br />
+                <p><b>Bienvenidos, este sito conciste en una plataforma de intercambios sin intermediarios. Donde las
+                        persoanas podran intercambiar libremente lo que deseen.
+                        Para ello en un futuro implementaremos el uso de la tecnologia de Contratos Inteligentes o
+                        <span> SamrtContract</span>, pero que actualemte no se encuentra disponible estamos trabajando
+                        en su
+                        implementacion.</b>
+                </p>
+                <br />
+                <p><b>Por el contrario, podras consultar y ponerte en contacto con los distintos ofertantes del sito a
+                        travez de email o telefono y pactar la forma de intercambio que desees.
+                        Ten en cuenta que no se permitira publicar articulos sin su debida documentacion; como ser:
+                        titulo, certificados,
+                        factura, etc. ni tampoco ningun articulo prohibido por las leyes actuales.</b>
+                </p>
+                <br />
+                <p><b>Si lo deseas te invito a que te des una vuelta por el Sitio y me dejes una opinion en los
+                        comentarios. Saludos!</b></p>
 
-                    <section className="testimonios">
-                        <h2>Comentarios</h2>
-                        <br />
-                        <div className="testimonio">
-                            <span className="cita">"Muy serios y profesionales."</span>
-                            <span className="autor">Pablo R.</span>
-                        </div>
-                    </section>
+            </section>
+
+            <section className="testimonios">
+                <h2>Comentarios</h2>
+                <br />
+                <div className="testimonio">
+                    <span className="cita">"Muy serios y profesionales."</span>
+                    <span className="autor">Pablo R.</span>
                 </div>
-            </div>
-        </main>
-    );
+            </section>
+        </div>
+    </div>
+</main>
+);
 }
 
 export default Home;
