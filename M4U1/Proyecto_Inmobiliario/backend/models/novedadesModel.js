832e7ff7a1cb1eac3abf4864d5a240970e9d6919
Hernan Carrizo <carrizohf35@gmail.com>
2023-01-16 21:44:42 -0300

    CRUD

--- M4U1/Proyecto_Inmobiliario/backend/models/novedadesModel.js
+++ M4U1/Proyecto_Inmobiliario/backend/models/novedadesModel.js
@@ -2,7 +2,6 @@ var pool = require('./bd'); //llamdando datos BD
 
 
 async function getNovedades() {
-
     var query = 'select * from novedades'
     var rows = await pool.query(query);
     return rows;
@@ -20,4 +19,31 @@ async function insertarNovedades(obj) {
     }
 }
 
-module.exports = { getNovedades, insertarNovedades }
+async function deleteNovedades(id) {
+    var query = 'delete from novedades where id = ?'
+    var rows = await pool.query(query, [id]);
+    return rows;
+}
+
+//novedad por id
+async function getNovedadesById(id) {
+    var query = 'select * from novedades where id = ?';
+    var rows = await pool.query(query, [id]);
+    return rows[0];
+}
+
+//update
+async function modificarNovedadesById(obj, id) {
+    try {
+        var query = 'update novedades set ? where id = ?';
+        var rows = await pool.query(query, [obj, id]);
+        return rows;
+
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+
+};
+
+module.exports = { getNovedades, insertarNovedades, deleteNovedades, getNovedadesById, modificarNovedadesById }
--- M4U1/Proyecto_Inmobiliario/backend/routes/admin/novedades.js
+++ M4U1/Proyecto_Inmobiliario/backend/routes/admin/novedades.js
@@ -23,7 +23,6 @@ router.get('/agregar', (req, res, next) => {
   });
 });
 
-
 router.post('/agregar', async (req, res, next) => {
   try {
     if (req.body.titulo != "" && req.body.subtitulo != "" && req.body.cuerpo != "") {
@@ -37,7 +36,6 @@ router.post('/agregar', async (req, res, next) => {
       })
     };
 
-
   } catch (error) {
     console.log(error);
     res.render('admin/agregar', {
@@ -48,5 +46,50 @@ router.post('/agregar', async (req, res, next) => {
     );
   }
 });
+
+// Eliminar
+router.get('/eliminar/:id', async (req, res, next) => {
+  var id = req.params.id;
+  await novedadesModel.deleteNovedades(id);
+  res.redirect('/admin/novedades');
+
+});
+
+//modificar
+router.get('/modificar/:id', async (req, res, next) => {
+  var id = req.params.id;
+  //console.log(req.params.id);
+  var novedades = await novedadesModel.getNovedadesById(id);
+
+  res.render('admin/modificar', {
+    layout: 'admin/layout',
+    novedades
+  })
+});
+
+//update
+router.post('/modificar', async (req, res, next) => {
+  try {
+    var obj = {
+      titulo: req.body.titulo,
+      subtitulo: req.body.subtitulo,
+      cuerpo: req.body.cuerpo
+    }
+    //console.log(obj);
+
+    await novedadesModel.modificarNovedadesById(obj, req.body.id);
+    res.redirect('/admin/novedades');
+
+  } catch (error) {
+    // console.log(error);
+    res.render('admin/modificar', {
+      layout: 'admin/layout',
+      error: true,
+      message: 'No se modifico la novedad'
+    });
+  }
+});
+
+
 
 module.exports = router;
